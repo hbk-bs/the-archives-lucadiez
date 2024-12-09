f343140bb7840a23fe2fa0b3783cd9c16da728f4
Luca Diez Aguilar <l.diez@hbk-bs.de>
2024-12-09 15:12:49 +0100

    Update index.js

--- crosswise/index.js
+++ crosswise/index.js
@@ -1,9 +1,41 @@
-
-
-function setup(){
-  const canvas = createCanvas(500,500);
-  canvas.parent("sketch");
-  background("black");
+class Particle {
+	x;
+	y;
+	constructor(_x, _y) {
+		this.x = _x;
+		this.y = _y;
+	}
+	display() {
+		circle(this.x, this.y, 10);
+	}
+	update() {
+		this.x = this.x + random(-1, 1);
+		this.y = this.y + random(10, 5);
+	}
 }
 
-function draw() {}
+const particles = [];
+function setup() {
+	const canvas = createCanvas(400, 400);
+	canvas.parent("sketch");
+	for (x = 0; x < width; x += 2) {
+		const p = new Particle(x, 0);
+		particles.push(p);
+	}
+}
+
+function draw() {
+  background(0)
+	for (let i = 0; i < particles.length; i++) {
+		const oneParticle = particles[i];
+		oneParticle.display();
+		oneParticle.update();
+	}
+
+  if(frameCount > 10){
+    noLoop();
+
+  }
+}
+
+function mouseDragged() {}
