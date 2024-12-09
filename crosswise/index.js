723019003b0ad3896f1670eb6903e5e1777b91b1
Luca Diez Aguilar <l.diez@hbk-bs.de>
2024-12-09 15:54:32 +0100

    changes crosswise

--- crosswise/index.js
+++ crosswise/index.js
@@ -6,36 +6,48 @@ class Particle {
 		this.y = _y;
 	}
 	display() {
-		circle(this.x, this.y, 10);
+		circle(this.x, this.y, 2);
 	}
 	update() {
-		this.x = this.x + random(-1, 1);
-		this.y = this.y + random(10, 5);
+		this.x = this.x + 1;
+		this.y = this.y + random(-1, 2);
 	}
 }
 
+class Particle2 {
+	x;
+	y;
+	constructor(_x, _y) {
+		this.x = _x;
+		this.y = _y;
+	}
+	display() {
+		circle(this.x, this.y, 2);
+	}
+	update() {
+		this.x = this.x + 1;
+		this.y = this.y + random(-2, 1);
+	}
+}
 const particles = [];
 function setup() {
 	const canvas = createCanvas(400, 400);
 	canvas.parent("sketch");
-	for (x = 0; x < width; x += 2) {
-		const p = new Particle(x, 0);
+	background("black")
+	noStroke();
+	for (y = 0; y < width/2; y += 20) {
+		const p = new Particle(0, y);
+		particles.push(p);
+	}
+	for (y = width/2; y < width+1; y += 20) {
+		const p = new Particle2(0, y);
 		particles.push(p);
 	}
 }
-
 function draw() {
-  background(0)
 	for (let i = 0; i < particles.length; i++) {
 		const oneParticle = particles[i];
 		oneParticle.display();
 		oneParticle.update();
 	}
-
-  if(frameCount > 10){
-    noLoop();
-
-  }
 }
-
-function mouseDragged() {}
