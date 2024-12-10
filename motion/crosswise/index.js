ee2be83a939c8550f20e43fc6647fa653e714087
Luca Diez Aguilar <l.diez@hbk-bs.de>
2024-12-10 11:48:51 +0100

    crosswise

--- motion/crosswise/index.js
+++ motion/crosswise/index.js
@@ -1,9 +1,13 @@
-class Particle {
+class line {
 	x;
 	y;
+	startX;
+	startY;
 	constructor(_x, _y) {
 		this.x = _x;
 		this.y = _y;
+		this.startX = _x;
+		this.startY = _y;
 	}
 	display() {
 		circle(this.x, this.y, 2);
@@ -14,12 +18,16 @@ class Particle {
 	}
 }
 
-class Particle2 {
+class line2 {
 	x;
 	y;
+	startX;
+	startY;
 	constructor(_x, _y) {
 		this.x = _x;
 		this.y = _y;
+		this.startX = _x;
+		this.startY = _y;
 	}
 	display() {
 		circle(this.x, this.y, 2);
@@ -29,25 +37,39 @@ class Particle2 {
 		this.y = this.y + random(-2, 1);
 	}
 }
-const particles = [];
+
+let widthfromrect = 0;
+const lines = [];
 function setup() {
 	const canvas = createCanvas(400, 400);
 	canvas.parent("sketch");
-	background("black")
+	background("black");
 	noStroke();
-	for (y = 0; y < width/2; y += 20) {
-		const p = new Particle(0, y);
-		particles.push(p);
+	for (y = 0; y < width / 2; y += 20) {
+		const p = new line(0, y);
+		lines.push(p);
 	}
-	for (y = width/2; y < width+1; y += 20) {
-		const p = new Particle2(0, y);
-		particles.push(p);
+	for (y = width / 2; y < width + 1; y += 20) {
+		const p = new line2(0, y);
+		lines.push(p);
 	}
 }
 function draw() {
-	for (let i = 0; i < particles.length; i++) {
-		const oneParticle = particles[i];
-		oneParticle.display();
-		oneParticle.update();
+	for (let i = 0; i < lines.length; i++) {
+		const oneLine = lines[i];
+		oneLine.display();
+		oneLine.update();
+
+		if (oneLine.x >= width) {
+			fill("black");
+			rect(0, 0, widthfromrect, height);
+			widthfromrect = widthfromrect + 401;
+
+			if (widthfromrect >= width) {
+				fill("white");
+				oneLine.x = oneLine.startX;
+				oneLine.y = oneLine.startY;
+			}
+		}
 	}
 }
